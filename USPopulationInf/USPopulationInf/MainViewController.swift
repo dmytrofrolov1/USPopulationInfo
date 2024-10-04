bd105e5a7dc5a03a5a9098d54e51389606c9f4e4
Dmytro <frolowdmitriy@gmail.com>
2024-10-04 14:32:05 +0100

    project init

---
+++ USPopulationInf/USPopulationInf/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  USPopulationInf
+//
+//  Created by Dmytro on 04.10.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
